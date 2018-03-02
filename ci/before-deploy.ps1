de31d3c71d5fee5549106401c4289026c9cec03e
Tim Oram <dev@mitmaro.ca>
2018-03-02 01:31:30 -0330

    WIP

--- ci/before-deploy.ps1
+++ ci/before-deploy.ps1
@@ -10,7 +10,7 @@ Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
 
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\interactive-rebase.exe" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\git-interactive-rebase.exe" '.\'
 
 7z a "$ZIP" *
 
